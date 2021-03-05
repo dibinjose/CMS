fec6f8b2a2734b2722cd02251d4fbc54e5c28d61
dibinjose <dibinantonyjose@gmail.com>
2021-03-05 14:05:09 +0530

    Component pages added

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './Style.css';
 
 function App() {
---
+++ client/src/components/userRegistration.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function userRegistration() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
