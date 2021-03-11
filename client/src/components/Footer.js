8242e20c15db125be2c0a88841c1276640e8db41
dibinjose <dibinantonyjose@gmail.com>
2021-03-11 21:53:59 +0530

    Project Changed

--- client/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer() {
-  return (
-    <footer>
-      <p>Created and designed by Dibin Jose</p>
-    </footer>
-  );
-}
--- client/src/components/itemCreation.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function itemCreation() {
-    return (
-        <div>
-            
-        </div>
-    )
-}
